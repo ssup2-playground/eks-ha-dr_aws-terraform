825a95487b68be93b25033a0af354637a0fa6410
ssup2 <supsup5642@gmail.com>
2023-02-06 02:59:07 +0900

    first commit

---
+++ terraform-ha-single-cluster/main.tf
@@ -0,0 +1,97 @@
+## Provider
+provider "aws" {
+  region = local.region
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
+## Data
+data "aws_availability_zones" "available" {}
+
+## Locals
+locals {
+  name   = "eks-ha-single-cluster"
+  region = "us-east-2"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+}
+
+## VPC
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 10)]
+
+  enable_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  manage_default_network_acl    = true
+  manage_default_route_table    = true
+  manage_default_security_group = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+## EKS
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+
+  cluster_name                   = local.name
+  cluster_endpoint_public_access = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    manage = {
+      min_size     = 2
+      max_size     = 2
+      desired_size = 2
+
+      instance_types = ["m5.large"]
+
+      labels = {
+        nodetype = "manage"
+      }
+    }
+  }
+}
+
+## EFS
+module "efs" {
+  source = "terraform-aws-modules/efs/aws"
+
+  name = local.name
+
+  mount_targets         = { for k, v in zipmap(local.azs, module.vpc.private_subnets) : k => { subnet_id = v } }
+  security_group_vpc_id = module.vpc.vpc_id
+  security_group_rules = {
+    vpc = {
+      cidr_blocks = module.vpc.private_subnets_cidr_blocks
+    }
+  }
+}
+
+## Aurora
